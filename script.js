89940536f4b2059f2d9535ecdf527e447e4fcddd
alamoorubabaiah <alamurubabaiah@gmail.com>
2026-09-26 11:01:51 +0530

    updated meal description and images

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 // menubar:----
 let one11 = document.getElementById("one11")
-
 async function user() {
-
     let response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
     let data = await response.json()
     let result = data.categories.map((product) => {
         return `
             <div  class = "menu">
-          <button class="btn" onclick="vegfun('${product.strCategory}')">
-         <h6>${product.strCategory}</h6>
-        </button>
-                  </div>   `
-
+          <button class="btn" onclick="vegfun('${product.strCategory}')"> <h6>${product.strCategory}</h6> </button>
+         </div> `
     })
     one11.innerHTML += result.join("");
-
 }
 user()
 
@@ -29,28 +23,21 @@ async function fn() {
         <div class="items">
         <h6>${value.strCategory}</h6>
         <img src="${value.strCategoryThumb}" onclick="vegfun('${value.strCategory}')">
-        </div>
-        `
+        </div> `
     })
     cart.innerHTML += result.join("");
 }
 fn()
 
-
-
-
 /* filter */
-
 async function fdata() {
     let func = document.getElementById("func")
     let search = document.getElementById('Search').value.toLowerCase().trim()
     // console.log(search)//pasta
-
     if (search === "") {
         func.innerHTML = ""
         return
     }
-
     let res = await fetch(`http://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
     let d = await res.json()
     // console.log(d)//array of obj
@@ -61,113 +48,114 @@ async function fdata() {
           <p>${value.strArea}</p>
           <h5>${value.strMeal}</h5>
         </div> `
-
     })
     func.innerHTML = result1.join("")
-
-
 }
 
-
-
-// menu items while clicking
-/* let veg = document.getElementById("veg")
-async function vegfun(cate) {
-    window.open(
-        `second-page.html?category=${encodeURIComponent(cate)}`,
-        "_self"
-    );
-
-    let resolve2 = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
-    // console.log(resolve2);
-    let data1 = await resolve2.json()
-    let result2 = data1.categories.find((value1) => {
-
-        return value1.strCategory === category;
-
-
-    });
-    veg.innerHTML = result2
-
-}
- */
-
+// secondpage:-
 function vegfun(cate) {
-
-    window.open(
-        `second-page.html?category=${encodeURIComponent(cate)}`,
-        "_self"
-    );
-
+    window.open(`second-page.html?category=${encodeURIComponent(cate)}`,"_self");
 }
 
 async function vegan() {
 
     let sec = document.getElementById("sec");
-
-    // Get category from URL
     let params = new URLSearchParams(window.location.search);
-
     let category = params.get("category");
-
-    console.log(category);
-
-    // Get all categories
-    let response = await fetch(
-        "https://www.themealdb.com/api/json/v1/1/categories.php"
-    );
-
+    // console.log("Category:", category);
+    if (!category) {
+        sec.innerHTML = `<h2>No category selected</h2>`;
+        return;
+    }
+    let response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
     let data = await response.json();
-
     // Find selected category
-    let result = data.categories.find((items) => {
-        return items.strCategory === category;
+    let categoryData = data.categories.find((item) => {
+        return item.strCategory.toLowerCase() === category.toLowerCase();
     });
+    if (!categoryData) {
+        sec.innerHTML = `<h2>Category not found</h2>`;
+        return;
+    }
+//    for related meals
+    let mealResponse = await fetch( `https://www.themealdb.com/api/json/v1/1/filter.php?c=${encodeURIComponent(category)}`);
+    let mealData = await mealResponse.json();
 
-    if (!result) {
-        sec.innerHTML = "<h2>Category not found</h2>";
+    //    description
+    let description = `
+        <div class="category-description">
+            <h2>${categoryData.strCategory}</h2>
+            <h5>${categoryData.strCategoryDescription}</h5>
+        </div>`;
+
+    let mealsHeading = `
+        <div class="meals-title">
+            <h2>MEALS</h2>
+            <div class="title-line"1></div>
+        </div>
+    `;
+
+    if (!mealData.meals) {
+        sec.innerHTML = description + mealsHeading + `<h3>No meals found</h3>`;
         return;
     }
 
-    sec.innerHTML = `
-        <div class="flex">
+    let meals = mealData.meals.map((meal) => {
+        return `
+            <div class="meal-card">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal} "onclick="note('${meal.strMeal}')">
+                <h5> ${meal.strMeal}</h5>
+         </div>`;
+    });
 
-            <h2>${result.strCategory}</h2>
-
-           
-
-            <p>${result.strCategoryDescription}</p>
-
-            <hr>
-
+    sec.innerHTML = ` ${description} ${mealsHeading}
+        <div class="meal-grid">
+            ${meals.join("")}
         </div>
     `;
 }
-
 vegan();
 
+// thirdpage:-
+
+function note(three) {
+    window.open(`third-page.html?meal=${encodeURIComponent(three)}`, "_self");
+}
+
+async function getMeal(){
+    let params = new URLSearchParams(window.location.search);
+    let meal = params.get("meal");
+     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${encodeURIComponent(meal)}`);
+     let  data = await response.json();
+      let mealData = data.meals.find((item) => {
+        return item.strMeal.toLowerCase() === meal.toLowerCase();
+    });
+   
+       let name = `
+        <div class="meal-descrip">
+          <h4> 🏡>>${mealData.strMeal} </h4>
+        </div>`;
+
+        let img =`
+          <div class="img">
+        <img src="${mealData.strMealThumb}">
+    </div>
+        `
 
 
 
 
 
-// async function vegan(){
-//     let sec = document.getElementById("sec");
-//     // let search1 = document.getElementById("section1")
-//     let rej = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${}`);
-//     let rem = await rej.json();
-//     let date = rem.categories.map((items)=>{
-//         return `
-//         <div class = "flex">
-//         <h2>${items.strCategory}</h2>
-//         <p>${items.strCategoryDescription}</p> <hr>
+    ingrident.innerHTML = ` ${name} ${img}`
+    
 
-     
-//         </div> 
-//         `
-//     })
-//     sec.innerHTML = date.join();
-// }
+}
+getMeal();
+
+
+
+
+
 
 
 
